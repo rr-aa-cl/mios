3fbad9e994752dfde28797e6eabd9691e1ebbaef
Lars Johannsmeier <lars.johannsmeier@tum.de>
2020-08-04 06:05:27 -0700

    work in progress

--- ml_service/problem_definition/domain.py
+++ ml_service/problem_definition/domain.py
@@ -1,3 +1,12 @@
 class Domain:
     def __init__(self):
         self.limits = dict()
+        self.vector_mapping = list()
+        for p in self.limits.keys():
+            self.vector_mapping.append(p)
+
+    def normalize(self, parameters):
+        pass
+
+    def denormalize(self):
+        pass
---
+++ ml_service/services/basinhopping.py
@@ -0,0 +1,13 @@
+import logging
+from scipy import optimize
+
+from services.base_service import BaseService
+
+
+class BasinhoppingService(BaseService):
+    def __init__(self):
+        super().__init__()
+
+    def _initialize(self):
+        pass
+
