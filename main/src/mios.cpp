29f527cfd598d9a0a392af3af44f826802175c38
Lars Johannsmeier <lars.johannsmeier@tum.de>
2020-09-22 08:17:21 -0700

    v0.6.6.2

--- main/src/mios.cpp
+++ main/src/mios.cpp
@@ -46,7 +46,7 @@ int main(int argc, char** argv){
 
     spdlog::info("############################################################");
     spdlog::info("MIOS");
-    spdlog::info("Version: 0.6.6.1");
+    spdlog::info("Version: 0.6.6.2");
 
     unsigned port=12000;
     if(!msrm_utils::is_port_available("localhost",port)){
