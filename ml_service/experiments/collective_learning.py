8fb9a722878866b10e1097d10ef60aa2dd9e7f60
Lars Johannsmeier <lars.johannsmeier@tum.de>
2020-09-28 10:01:42 +0200

    added experiment files

---
+++ ml_service/experiments/collective_learning.py
@@ -0,0 +1,107 @@
+from problem_definition.domain import Domain
+from problem_definition.problem_definition import ProblemDefinition
+from problem_definition.problem_definition import CostFunction
+from task_scheduler.creation_pipeline import CreationPipeline
+from services.cmaes import CMAESConfiguration
+from definitions import insert_cylinder_30
+from definitions import insert_cylinder_30_cost
+
+
+def insert_cylinder_10():
+    pd = insert_cylinder_30()
+    pd.default_context["parameters"]["insertable"] = "cylinder_10"
+    pd.default_context["parameters"]["insert_intro"] = "hole_10"
+    pd.default_context["parameters"]["insert_approach"] = "hole_10_above"
+    pd.default_context["skills"]["insertion"]["skill"]["ROI_x"] = [-0.01, 0.01, -0.01, 0.01, -1, 1]
+    pd.reset_instructions[0]["parameters"]["parameters"]["extractable"] = "cylinder_10"
+    pd.reset_instructions[0]["parameters"]["parameters"]["extract_from"] = "hole_10"
+    pd.reset_instructions[0]["parameters"]["parameters"]["extract_to"] = "hole_10_above"
+    return pd
+
+
+def insert_cylinder_20():
+    pd = insert_cylinder_30()
+    pd.default_context["parameters"]["insertable"] = "cylinder_20"
+    pd.default_context["parameters"]["insert_intro"] = "hole_20"
+    pd.default_context["parameters"]["insert_approach"] = "hole_20_above"
+    pd.default_context["skills"]["insertion"]["skill"]["ROI_x"] = [-0.02, 0.02, -0.02, 0.02, -1, 1]
+    pd.reset_instructions[0]["parameters"]["parameters"]["extractable"] = "cylinder_20"
+    pd.reset_instructions[0]["parameters"]["parameters"]["extract_from"] = "hole_20"
+    pd.reset_instructions[0]["parameters"]["parameters"]["extract_to"] = "hole_20_above"
+    return pd
+
+
+def insert_cylinder_40():
+    pd = insert_cylinder_30()
+    pd.default_context["parameters"]["insertable"] = "cylinder_40"
+    pd.default_context["parameters"]["insert_intro"] = "hole_40"
+    pd.default_context["parameters"]["insert_approach"] = "hole_40_above"
+    pd.default_context["skills"]["insertion"]["skill"]["ROI_x"] = [-0.04, 0.04, -0.04, 0.04, -1, 1]
+    pd.reset_instructions[0]["parameters"]["parameters"]["extractable"] = "cylinder_40"
+    pd.reset_instructions[0]["parameters"]["parameters"]["extract_from"] = "hole_40"
+    pd.reset_instructions[0]["parameters"]["parameters"]["extract_to"] = "hole_40_above"
+    return pd
+
+def insert_cylinder_50():
+    pd = insert_cylinder_30()
+    pd.default_context["parameters"]["insertable"] = "cylinder_50"
+    pd.default_context["parameters"]["insert_intro"] = "hole_50"
+    pd.default_context["parameters"]["insert_approach"] = "hole_50_above"
+    pd.default_context["skills"]["insertion"]["skill"]["ROI_x"] = [-0.05, 0.05, -0.05, 0.05, -1, 1]
+    pd.reset_instructions[0]["parameters"]["parameters"]["extractable"] = "cylinder_50"
+    pd.reset_instructions[0]["parameters"]["parameters"]["extract_from"] = "hole_50"
+    pd.reset_instructions[0]["parameters"]["parameters"]["extract_to"] = "hole_50_above"
+    return pd
+
+
+def insert_cylinder_60():
+    pd = insert_cylinder_30()
+    pd.default_context["parameters"]["insertable"] = "cylinder_60"
+    pd.default_context["parameters"]["insert_intro"] = "hole_60"
+    pd.default_context["parameters"]["insert_approach"] = "hole_60_above"
+    pd.default_context["skills"]["insertion"]["skill"]["ROI_x"] = [-0.06, 0.06, -0.06, 0.06, -1, 1]
+    pd.reset_instructions[0]["parameters"]["parameters"]["extractable"] = "cylinder_60"
+    pd.reset_instructions[0]["parameters"]["parameters"]["extract_from"] = "hole_60"
+    pd.reset_instructions[0]["parameters"]["parameters"]["extract_to"] = "hole_60_above"
+    return pd
+
+
+def insert_key_abus():
+    pd = insert_cylinder_30()
+    pd.default_context["parameters"]["insertable"] = "key_abus_e30"
+    pd.default_context["parameters"]["insert_intro"] = "lock_abus_e30"
+    pd.default_context["parameters"]["insert_approach"] = "lock_abus_e30_above"
+    pd.default_context["skills"]["insertion"]["skill"]["ROI_x"] = [-0.01, 0.01, -0.01, 0.01, -1, 1]
+    pd.reset_instructions[0]["parameters"]["parameters"]["extractable"] = "key_abus_e30"
+    pd.reset_instructions[0]["parameters"]["parameters"]["extract_from"] = "lock_abus_e30"
+    pd.reset_instructions[0]["parameters"]["parameters"]["extract_to"] = "lock_abus_e30_above"
+    return pd
+
+
+def insert_plug_usb_c():
+    pd = insert_cylinder_30()
+    pd.default_context["parameters"]["insertable"] = "plug_usb_c"
+    pd.default_context["parameters"]["insert_intro"] = "slot_usb_c"
+    pd.default_context["parameters"]["insert_approach"] = "slot_usb_c_above"
+    pd.default_context["skills"]["insertion"]["skill"]["ROI_x"] = [-0.01, 0.01, -0.01, 0.01, -1, 1]
+    pd.reset_instructions[0]["parameters"]["parameters"]["extractable"] = "plug_usb_c"
+    pd.reset_instructions[0]["parameters"]["parameters"]["extract_from"] = "slot_usb_c"
+    pd.reset_instructions[0]["parameters"]["parameters"]["extract_to"] = "slot_usb_c_above"
+    return pd
+
+
+def create_tasks():
+    config = CMAESConfiguration()
+
+    c = CreationPipeline()
+    c.create_tasks_from_template(insert_cylinder_30(), config, 10, "collective-panda-007.local",
+                                 ["collective-panda-007.local"], "local")
+    c.create_tasks_from_template(insert_cylinder_10(), config, 10, "collective-panda-009.local",
+                                 ["collective-panda-009.local"], "local")
+    c.create_tasks_from_template(insert_cylinder_60(), config, 10, "collective-panda-001.local",
+                                 ["collective-panda-001.local"], "local")
+    c.create_tasks_from_template(insert_key_abus(), config, 10, "collective-panda-002.local",
+                                 ["collective-panda-002.local"], "local")
+    c.create_tasks_from_template(insert_plug_usb_c(), config, 10, "collective-panda-003.local",
+                                 ["collective-panda-003.local"], "local")
+    return c
