829b21d8e61bed387307d8a8bad7656237c6bd3f
Lars Johannsmeier <lars.johannsmeier@tum.de>
2020-10-05 11:25:46 +0200

    removed unused code

--- bin/_mios.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-
-ROOT=$(dirname "$(realpath $0)")
-
-unset LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=${ROOT}/../lib:${ROOT}/../lib/primitives:${ROOT}/../lib/skills:${ROOT}/../lib/tasks:${ROOT}/../lib/plugins:${ROOT}/../lib/led_patterns:${ROOT}/../../dependencies
-exec ${ROOT}/mios
--- bin/mios.sh
+++ bin/mios.sh
@@ -1,12 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
 ARG1="$1"
 ROOT=$(dirname "$(realpath $0)")
 PYTHONPATH=$PYTHONPATH:${ROOT}/../python/desk:${ROOT}/../ml_service:/usr/local/lib/python3.6/dist-packages
 export PYTHONPATH
 export ROS_MASTER_URI=http://localhost:11311
-source /opt/ros/melodic/setup.bash
+#source /opt/ros/melodic/setup.bash
 roscore &
 unset LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=${ROOT}/../lib:${ROOT}/../lib/plugins:/opt/ros/melodic/lib:${ROOT}/../../dependencies
 exec ${ROOT}/mios $ARG1
+exit 0
