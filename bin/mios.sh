28de405a186de065d2af9cde17aa5631eae5bc33
Lars Johannsmeier <lars.johannsmeier@tum.de>
2020-10-09 10:55:52 +0200

    removed automatic start of roscore
    Former-commit-id: 4d35cae64ea2236521c2eafc8c24fbe89f276c53

--- bin/mios.sh
+++ bin/mios.sh
@@ -5,8 +5,6 @@ ROOT=$(dirname "$(realpath $0)")
 PYTHONPATH=$PYTHONPATH:${ROOT}/../python/desk:${ROOT}/../ml_service:/usr/local/lib/python3.6/dist-packages
 export PYTHONPATH
 export ROS_MASTER_URI=http://localhost:11311
-#source /opt/ros/melodic/setup.bash
-roscore &
 unset LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=${ROOT}/../lib:${ROOT}/../lib/plugins:/opt/ros/melodic/lib:${ROOT}/../../dependencies
 exec ${ROOT}/mios $ARG1
