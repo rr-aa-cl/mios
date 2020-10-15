9283790208d2000a0c5fff422d0a34141babac88
ga25mas <samuel.schneider@tum.de>
2020-10-15 09:09:43 +0200

    add random forest
    Former-commit-id: be2a317c79d3df8a5891bc21a373e50ee9f883c6

---
+++ ml_service/knowledge_processor/kg_random_forest.py
@@ -0,0 +1,15 @@
+from knowledge_processor.knowledge_generalizer_base import KnowledgeGeneralizerBase
+from sklearn.ensemble import RandomForestRegressor
+import numpy as np
+
+
+class KGRandomForest(KnowledgeGeneralizerBase):
+    def __init__(self):
+        super().__init__()
+        self.regr = RandomForestRegressor(max_depth=2, random_state=0)
+
+    def fit_data(self, x: np.ndarray, y: np.ndarray):
+        self.regr.fit(x, y)
+
+    def predict_data(self, x: np.ndarray):
+        return self.regr.predict(x.reshape(1, -1))
