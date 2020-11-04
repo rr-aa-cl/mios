5fd5f7cca77083ba82ea4da6d8c6d470e35031c0
Lars Johannsmeier <lars.johannsmeier@tum.de>
2020-11-04 17:08:24 +0100

    updates
    Former-commit-id: 971805a7a39b01cf4f75378022794fc0d8e0d097

--- ml_service/run_experiments.py
+++ ml_service/run_experiments.py
@@ -23,15 +23,17 @@ def transfer_learning_10(from_host: str = None, from_db: str = None, task: str =
     service_config.n_ind = 10
     service_config.n_gen = 10
     knowledge = None
+    tags = ["transfer_learning"]
     if from_host is not None:
         knowledge = {
             "mode": "specific",
             "kb_location": from_host,
             "kb_db": from_db,
             "kb_task_type": task,
-            "kb_tags": ["transfer_learning", from_tag]
+            "kb_tags": ["transfer_learning", from_tag],
+            tags: ["transfer_learning", "from_" + from_tag]
         }
-    start_experiment("collective-panda-007", pd, service_config, 10, tags=["transfer_learning", "from_" + from_tag], knowledge=knowledge)
+    start_experiment("collective-panda-007", pd, service_config, 10, tags=tags, knowledge=knowledge)
 
 
 def transfer_learning_20():
