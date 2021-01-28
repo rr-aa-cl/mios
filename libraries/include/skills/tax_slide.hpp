52393ebb32c11164b8a1a76979e42ae0bccf134a
Lars Johannsmeier <lars.johannsmeier@tum.de>
2021-01-28 11:39:56 +0100

    added slide skill
    Former-commit-id: 42ad5c75cdb34cd4a807ffaa8204a5d7cf66c145

---
+++ libraries/include/skills/tax_slide.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "skill/skill.hpp"
+namespace mios{
+class SkillParametersTaxSlide : public SkillParameters{
+public:
+    bool from_json(const nlohmann::json& parameters) override;
+    std::map<std::string, std::set<std::string> > get_parameter_list() override;
+    Eigen::Matrix<double,2,1> speed;
+    Eigen::Matrix<double,2,1> acc;
+    double f_slide;
+};
+
+class TaxSlide : public Skill{
+public:
+    TaxSlide(const std::string& name, Memory* memory, Portal* portal);
+    Eigen::Matrix<double,3,3> get_O_R_T_0(const Percept& p) const override;
+
+private:
+    std::shared_ptr<ManipulationPrimitive> get_initial_mp(const Percept& p_0) override;
+    bool check_local_pre_conditions(const Percept &p) override;
+    bool check_local_suc_conditions(const Percept& p) override;
+    bool check_local_err_conditions(const Percept &p) override;
+
+    std::shared_ptr<ManipulationPrimitive> create_slide_mp(const Percept& p);
+
+private:
+
+};
+}
