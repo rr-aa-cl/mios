24fb24b9ad8c77299fd820a8487cf21952eac8cb
Lars Johannsmeier <lars.johannsmeier@tum.de>
2021-09-29 03:44:34 -0700

    added slide open skill
    Former-commit-id: 367f20a7df73dea01c1bd15421f4b743e29c5320

---
+++ src/libraries/include/mios/skills/tax_slide_open.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "mios/skill/skill.hpp"
+namespace mios{
+class SkillParametersTaxSlideOpen : public SkillParameters{
+public:
+    bool from_json(const nlohmann::json& parameters) override;
+    std::map<std::string, std::set<std::string> > get_parameter_list() override;
+
+    struct P0{
+        Eigen::Matrix<double,6,1> K_x;
+        Eigen::Matrix<double,2,1> dX_d;
+        Eigen::Matrix<double,2,1> ddX_d;
+    }p0;
+
+    struct P1{
+        Eigen::Matrix<double,6,1> K_x;
+        Eigen::Matrix<double,2,1> dX_d;
+        Eigen::Matrix<double,2,1> ddX_d;
+        double f_slide;
+    }p1;
+
+    struct P2{
+        Eigen::Matrix<double,6,1> K_x;
+        Eigen::Matrix<double,2,1> dX_d;
+        Eigen::Matrix<double,2,1> ddX_d;
+        double f_slide;
+    }p2;
+};
+
+class TaxSlideOpen : public Skill{
+public:
+    TaxSlideOpen(const std::string& name, Memory* memory, Portal* portal);
+    Eigen::Matrix<double,3,3> get_O_R_T_0(const Percept& p) const override;
+
+private:
+    std::shared_ptr<ManipulationPrimitive> get_initial_mp(const Percept& p_0) override;
+    std::optional<std::shared_ptr<ManipulationPrimitive> > graph_transition(const Percept &p) override;
+    bool check_local_pre_conditions(const Percept &p) override;
+    bool check_local_suc_conditions(const Percept& p) override;
+    bool check_local_ex_conditions(const Percept &p) override;
+    bool check_local_err_conditions(const Percept &p) override;
+
+    void update_internal_models(const Percept &p) override;
+
+    std::shared_ptr<ManipulationPrimitive> create_approach_mp(const Percept& p);
+    std::shared_ptr<ManipulationPrimitive> create_contact_mp(const Percept& p);
+    std::shared_ptr<ManipulationPrimitive> create_wipe_mp(const Percept& p);
+
+};
+}
---
+++ src/libraries/src/skills/tax_slide_open.cpp
@@ -0,0 +1,181 @@
+#include "mios/skills/tax_slide_open.hpp"
+#include "mios/strategies/move_to_pose.hpp"
+#include "mios/strategies/cart_compliance_strategy.hpp"
+#include "mios/strategies/twist_strategy.hpp"
+#include "mios/strategies/ff_strategy.hpp"
+
+#include "msrm_cpp_utils/math/math.hpp"
+
+namespace mios{
+
+bool SkillParametersTaxSlideOpen::from_json(const nlohmann::json& parameters){
+    if(parameters.find("p0")==parameters.end()){
+        spdlog::error("Parameters for primitive 0 are missing.");
+        return false;
+    }else if(parameters.find("p0")!=parameters.end()){
+        if(!msrm_utils::read_json_param<double,6,1>(parameters["p0"],"K_x",p0.K_x)){
+            spdlog::error("Missing parameter: p0.K_x");
+            return false;
+        }
+        if(!msrm_utils::read_json_param<double,2,1>(parameters["p0"],"dX_d",p0.dX_d)){
+            spdlog::error("Missing parameter: p0.dX_d");
+            return false;
+        }
+        if(!msrm_utils::read_json_param<double,2,1>(parameters["p0"],"ddX_d",p0.ddX_d)){
+            spdlog::error("Missing parameter: p0.ddX_d");
+            return false;
+        }
+    }
+
+    if(parameters.find("p1")==parameters.end()){
+        spdlog::error("Parameters for primitive 1 are missing.");
+        return false;
+    }else if(parameters.find("p1")!=parameters.end()){
+        if(!msrm_utils::read_json_param<double,6,1>(parameters["p1"],"K_x",p1.K_x)){
+            spdlog::error("Missing parameter: p1.K_x");
+            return false;
+        }
+        if(!msrm_utils::read_json_param<double,2,1>(parameters["p1"],"dX_d",p1.dX_d)){
+            spdlog::error("Missing parameter: p1.dX_d");
+            return false;
+        }
+        if(!msrm_utils::read_json_param<double,2,1>(parameters["p1"],"ddX_d",p1.ddX_d)){
+            spdlog::error("Missing parameter: p1.ddX_d");
+            return false;
+        }
+        if(!msrm_utils::read_json_param(parameters["p1"],"f_slide",p1.f_slide)){
+            spdlog::error("Missing parameter: p1.f_slide");
+            return false;
+        }
+    }
+
+    if(parameters.find("p2")==parameters.end()){
+        spdlog::error("Parameters for primitive 2 are missing.");
+        return false;
+    }else if(parameters.find("p2")!=parameters.end()){
+        if(!msrm_utils::read_json_param<double,6,1>(parameters["p2"],"K_x",p2.K_x)){
+            spdlog::error("Missing parameter: p2.K_x");
+            return false;
+        }
+        if(!msrm_utils::read_json_param<double,2,1>(parameters["p2"],"dX_d",p2.dX_d)){
+            spdlog::error("Missing parameter: p2.dX_d");
+            return false;
+        }
+        if(!msrm_utils::read_json_param<double,2,1>(parameters["p2"],"ddX_d",p2.ddX_d)){
+            spdlog::error("Missing parameter: p2.ddX_d");
+            return false;
+        }
+        if(!msrm_utils::read_json_param(parameters["p1"],"f_slide",p1.f_slide)){
+            spdlog::error("Missing parameter: p1.f_slide");
+            return false;
+        }
+    }
+    return true;
+}
+
+std::map<std::string, std::set<std::string> > SkillParametersTaxSlideOpen::get_parameter_list(){
+    return {{"p0",{"K_x","dX_d","ddX_d"}},{"p1",{"K_x","dX_d","ddX_d","f_slide"}},{"p2",{"K_x","dX_d","ddX_d","f_slide"}}};
+}
+
+TaxSlideOpen::TaxSlideOpen(const std::string& name, Memory* memory, Portal* portal):Skill("TaxSlideOpen",{"Approach","Container"},name,memory,portal,{ControlMode::mCartTorque}){
+
+}
+
+Eigen::Matrix<double,3,3> TaxSlideOpen::get_O_R_T_0(const Percept &p) const{
+    return get_object("Container")->O_T_OB.block<3,3>(0,0);
+}
+
+std::shared_ptr<ManipulationPrimitive> TaxSlideOpen::get_initial_mp(const Percept& p){
+    return create_approach_mp(p);
+}
+
+std::optional<std::shared_ptr<ManipulationPrimitive> > TaxSlideOpen::graph_transition(const Percept &p){
+    if(get_active_mp()->get_name()=="approach"){
+        if(get_active_mp()->get_strategy_interface("move")->finished()){
+            return create_contact_mp(p);
+        }else{
+            return {};
+        }
+    }
+    if(get_active_mp()->get_name()=="contact"){
+        if(p.proprioception.TF_F_ext_K(2)>m_memory->read_parameters()->user.F_ext_contact(0)){
+            return create_wipe_mp(p);
+        }else{
+            return {};
+        }
+    }
+    return {};
+}
+
+std::shared_ptr<ManipulationPrimitive> TaxSlideOpen::create_approach_mp(const Percept &p){
+    spdlog::trace("TaxWipe::create_approach_mp()");
+    std::shared_ptr<SkillParametersTaxSlideOpen> skill_params = get_parameters<SkillParametersTaxSlideOpen>();
+    std::shared_ptr<ManipulationPrimitive> mp = create_mp("approach",p);
+    mp->create_strategy<MoveToPoseStrategy>("move",1);
+    std::shared_ptr<MoveToPoseStrategy> move = mp->get_strategy<MoveToPoseStrategy>("move");
+    Eigen::Matrix<double,4,4> T_a = get_object_pose_T("Approach");
+    move->set_goal(T_a,skill_params->p0.dX_d,skill_params->p0.ddX_d);
+    mp->create_strategy<CartComplianceStrategy>("compliance",1);
+    mp->get_strategy<CartComplianceStrategy>("compliance")->set_complicance(skill_params->p0.K_x,m_memory->read_parameters()->control.cart_imp.xi_x);
+    return mp;
+}
+
+std::shared_ptr<ManipulationPrimitive> TaxSlideOpen::create_contact_mp(const Percept &p){
+    spdlog::trace("TaxWipe::create_contact_mp");
+    std::shared_ptr<SkillParametersTaxSlideOpen> skill_params = get_parameters<SkillParametersTaxSlideOpen>();
+    std::shared_ptr<ManipulationPrimitive> mp = create_mp("contact",p);
+    mp->create_strategy<TwistStrategy>("move",1);
+    std::shared_ptr<TwistStrategy> move = mp->get_strategy<TwistStrategy>("move");
+    Eigen::Matrix<double,6,1> dX_d;
+    Eigen::Matrix<double,3,1> dir=get_object_pose_T("Surface").block<3,1>(0,3)-p.proprioception.T_T_EE.block<3,1>(0,3);;
+    dir/=dir.norm();
+    dX_d<<dir*skill_params->p1.dX_d(0),0,0,0;
+    move->set_TF_dX_d(dX_d,skill_params->p1.ddX_d);
+    mp->create_strategy<CartComplianceStrategy>("compliance",1);
+    mp->get_strategy<CartComplianceStrategy>("compliance")->set_complicance(skill_params->p1.K_x,m_memory->read_parameters()->control.cart_imp.xi_x);
+    return mp;
+}
+
+std::shared_ptr<ManipulationPrimitive> TaxSlideOpen::create_wipe_mp(const Percept &p){
+    spdlog::trace("TaxWipe::create_slide_mp()");
+    std::shared_ptr<SkillParametersTaxSlideOpen> skill_params = get_parameters<SkillParametersTaxSlideOpen>();
+    std::shared_ptr<ManipulationPrimitive> mp = create_mp("pull",p);
+    mp->create_strategy<MoveToPoseStrategy>("slide",1);
+    mp->create_strategy<FFStrategy>("push",1);
+    std::shared_ptr<MoveToPoseStrategy> slide = mp->get_strategy<MoveToPoseStrategy>("slide");
+    slide->set_goal(get_object_pose_T("GoalPose"),skill_params->p2.dX_d,skill_params->p2.ddX_d);
+    std::shared_ptr<FFStrategy> push = mp->get_strategy<FFStrategy>("push");
+    Eigen::Matrix<double,6,1> F_d;
+    F_d<<0,0,skill_params->p2.f_slide,0,0,0;
+    push->set_TF_F_ff(F_d,m_memory->read_parameters()->limits.cartesian_space.dF_J_max);
+    mp->create_strategy<CartComplianceStrategy>("compliance",1);
+    mp->get_strategy<CartComplianceStrategy>("compliance")->set_complicance(skill_params->p2.K_x,m_memory->read_parameters()->control.cart_imp.xi_x);
+    return mp;
+}
+
+bool TaxSlideOpen::check_local_pre_conditions(const Percept &p){
+    return true;
+}
+
+bool TaxSlideOpen::check_local_err_conditions(const Percept &p){
+    return false;
+}
+
+void TaxSlideOpen::update_internal_models(const Percept &p){
+    if(p.proprioception.TF_F_ext_K.block<3,1>(0,0).norm()>m_memory->read_parameters()->user.F_ext_contact(0)){
+        m_memory->get_object("Wipeable")->set_pose(p.proprioception.O_T_EE(0,3),p.proprioception.O_T_EE(1,3),p.proprioception.O_T_EE(2,3),{});
+    }
+}
+
+bool TaxSlideOpen::check_local_suc_conditions(const Percept &p){
+    return true;
+}
+
+bool TaxSlideOpen::check_local_ex_conditions(const Percept &p){
+    if(get_active_mp()->get_name()=="wipe"){
+        return get_active_mp()->get_strategy_interface("move")->finished();
+    }
+    return false;
+}
+
+}
