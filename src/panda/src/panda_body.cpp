76bb3205eac907e65bfd77bf5657b71bb39a2aa9
Samuel Schneider <samuel.schneider@tum.de>
2023-02-13 09:50:18 +0100

    connecti to right db (left/right)
    Former-commit-id: 37fe2a93dc6fcf148f4e3b09cf87ea042cabc575

--- src/panda/src/panda_body.cpp
+++ src/panda/src/panda_body.cpp
@@ -281,7 +281,7 @@ bool PandaBody::pre_run_checks() const{
 }
 
 bool PandaBody::is_robot(const std::string &ip){
-    spdlog::trace("PandaBody::is_robot()"+ip+m_memory->get_parameters()->system.desk_user+ m_memory->get_parameters()->system.desk_pwd);
+    spdlog::trace("PandaBody::is_robot()"+ip+m_memory->get_parameters()->system.desk_user+ m_memory->get_parameters()->system.desk_pwd+m_memory->m_robot_arm);
     //int tokenForceTimeout = 0;
     try{
         pybind11::module desk_client = pybind11::module::import("desk_client");
