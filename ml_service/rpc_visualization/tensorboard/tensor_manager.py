481ee861f071b537c38c74bd4f47a76ddc2e1d46
Samuel Schneider <samuel.schneider@tum.de>
2023-09-27 10:51:52 +0200

    update tesor_manager for new ml_service interface
    Former-commit-id: b54e513dbcca685f9a8c46e105d51cffe376125d

--- ml_service/rpc_visualization/tensorboard/tensor_manager.py
+++ ml_service/rpc_visualization/tensorboard/tensor_manager.py
@@ -13,32 +13,39 @@ from xmlrpc.server import SimpleXMLRPCServer
 
 robot_list = ["10.157.175.221", "10.157.174.166", "10.157.174.167", "10.157.174.168", "10.157.174.89" , "10.157.174.80" , "10.157.174.200", "10.157.175.129", "10.157.174.36" , "10.157.174.59", "10.157.175.87", "10.157.174.241", "10.157.174.201", "10.157.174.247", "10.157.174.202", "10.157.174.203", "10.157.174.46", "10.157.174.103", "10.157.174.206", "10.157.175.173", "10.157.174.244", "10.157.174.205", "10.157.175.156", "10.157.174.186","10.157.174.245", "10.157.174.249", "10.157.174.255", "10.157.174.42" , "10.157.174.148", "10.157.175.134"]
 
+receiving_ip = "10.0.2.32"
+receiving_port = 8004
+
 def send_start():
     for r in robot_list:
         try: 
-            server = xmlrpc.client.ServerProxy("http://%s:%s/" %(r, "8002"))
-            server.send_start()
-            print(r)
+            server = xmlrpc.client.ServerProxy("http://%s:%s/" %(r, "8000"))
+            result = server.start_telemetry(receiving_ip,receiving_port)
+            if not result:
+                print("subscribe to telemetry failed for ", r)
+            else:
+                print("successfully subscribed to ",r)
         except:
             print("error")
 
 def send_stop():
     for r in robot_list:
         try:
-            server = xmlrpc.client.ServerProxy("http://%s:%s/" %(r, "8002"))
-            server.send_off()
+            server = xmlrpc.client.ServerProxy("http://%s:%s/" %(r, "8000"))
+            server.stop_telemetry()
             print(r)
         except:
             print("error")
  
 
 def empty_buffer():
-    for r in robot_list:
-        try:
-            server = xmlrpc.client.ServerProxy("http://%s:%s/" %(r, "8002"))
-            server.empty_buffer()
-        except:
-            print("error")
+    print("not implemented")
+    #for r in robot_list:
+    #   try:
+    #        server = xmlrpc.client.ServerProxy("http://%s:%s/" %(r, "8000"))
+    #        server.empty_buffer()
+    #    except:
+    #        print("error")
 
         
 def print_all():
