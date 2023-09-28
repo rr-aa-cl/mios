a83877724180275ff523f4c0e1d8a3d4b0908d8d
popnut <ge82kop@mytum.de>
2023-09-28 17:38:56 +0200

    update transfermap based on cost
    Former-commit-id: 61dff49768b4641c2f4eddfaecd36b8c66f76294

--- ml_service/rpc_visualization/animation/transfer_ani.py
+++ ml_service/rpc_visualization/animation/transfer_ani.py
@@ -7,25 +7,48 @@ import seaborn as sns
 
 
 
+# def sns_heatmap(i , m):
+#     color_list = ["#F08080","#E7F1E7","#D4E6D4","#BDD7BD","#A7CBA7","#8DBB8D","#78AE78","#5E9E5E","#479147","#308030","#197519","#016701"]
+#     cmap = colors.ListedColormap(color_list)
+#     # labels = ["task"+str(i) for i in range(1,26)]
+#     labels = ["IEC(C7)", "Triangle-1", "Hexagon-1", "USB-1", "Triangle-2", "Cylinder-1", "Key-1", "Plug(type F)-1", "Audio Jack(3.5mm)", "IEC(C13)", "Cylinder-2", "Hexagon-2", "HDMI-1", "Key-2", "Cylinder-3", "Square-1", "Hexagon-3", "Square-2", "Audio jack(6.35mm)", "USB-2", "Plug(type C)", "Key-3", "Plug(type F)-2", "HDMI-2", "Key-4"]
+#     # bounds = [1, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5]
+#     bounds = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#     norm = colors.BoundaryNorm(bounds, cmap.N)
+#     plt.clf()
+#     # c = sns.color_palette("light:#006600", as_cmap=True)
+#     ax = sns.heatmap(m[i], linewidth=0.5, cmap=cmap, norm=norm, annot=True)
+
+#     ax.set_xticklabels(labels, rotation=45)    
+#     ax.set_yticklabels(labels, rotation=45)  
+#     # ax.set_yticks([])  # Remove y labels
+#     # plt.title("Collective Learning Process")
+
+#     ax.set_title(f'Collective Learning Process')
+
+
 def sns_heatmap(i , m):
-    color_list = ["#F08080","#E7F1E7","#D4E6D4","#BDD7BD","#A7CBA7","#8DBB8D","#78AE78","#5E9E5E","#479147","#308030","#197519","#016701"]
+    color_list = ["#E7F1E7","#D4E6D4","#BDD7BD","#A7CBA7","#8DBB8D","#78AE78","#5E9E5E","#479147","#308030","#197519","#016701"]
+    color_list.reverse()
     cmap = colors.ListedColormap(color_list)
-    # labels = ["task"+str(i) for i in range(1,26)]
-    labels = ["IEC(C7)", "Triangle-1", "Hexagon-1", "USB-1", "Triangle-2", "Cylinder-1", "Key-1", "Plug(type F)-1", "Audio Jack(3.5mm)", "IEC(C13)", "Cylinder-2", "Hexagon-2", "HDMI-1", "Key-2", "Cylinder-3", "Square-1", "Hexagon-3", "Square-2", "Audio jack(6.35mm)", "USB-2", "Plug(type C)", "Key-3", "Plug(type F)-2", "HDMI-2", "Key-4"]
-    # bounds = [1, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5]
-    bounds = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    norm = colors.BoundaryNorm(bounds, cmap.N)
+    bounds = [0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.25, 1.5, 1.75, 2.0, 2.5, 3.0]
+    # bounds = list(range(11))
+    norm = colors.BoundaryNorm(bounds, cmap.N)    # labels = ["task"+str(i) for i in range(1,26)]
+    labels = ["IEC(C7)", "Triangle-1", "Hexagon-1", "USB-1", "Triangle-2", "Cylinder-1", "Key-1", "Plug(type F)-1", "Audio Jack\n(3.5mm)", "IEC(C13)", "Cylinder-2", "Hexagon-2", "HDMI-1", "Key-2", "Cylinder-3", "Square-1", "Hexagon-3", "Square-2", "Audio jack\n(6.35mm)", "USB-2", "Plug(type C)", "Key-3", "Plug(type F)-2", "HDMI-2", "Key-4"]
+
+
     plt.clf()
     # c = sns.color_palette("light:#006600", as_cmap=True)
-    ax = sns.heatmap(m[i], linewidth=0.5, cmap=cmap, norm=norm, annot=True)
+    ax = sns.heatmap(m[i], linewidth=0.5, cmap=cmap, norm=norm, annot=True, fmt=".1f") #fmt="g"
 
-    ax.set_xticklabels(labels, rotation=45)    
-    ax.set_yticklabels(labels, rotation=45)  
+    ax.set_xticklabels(labels, rotation=90)    
+    ax.set_yticklabels(labels, rotation=0)  
+    cbar = ax.collections[0].colorbar
+    cbar.set_ticks(bounds)
     # ax.set_yticks([])  # Remove y labels
     # plt.title("Collective Learning Process")
 
     ax.set_title(f'Collective Learning Process')
-
  
 
 
