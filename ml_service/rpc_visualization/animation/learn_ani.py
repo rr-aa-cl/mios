801d6520a0a82fefb599b2a4d3de56cfdae31a92
popnut <ge82kop@mytum.de>
2023-09-28 17:12:21 +0200

    heatmap add task name
    Former-commit-id: ad7d521d64bf16de7014c2d2e71b38dc1dd9d086

--- ml_service/rpc_visualization/animation/learn_ani.py
+++ ml_service/rpc_visualization/animation/learn_ani.py
@@ -18,14 +18,22 @@ def generate_heatmap(frame_number):
 def sns_heatmap(i , m):
     color_list = ["#E7F1E7","#D4E6D4","#BDD7BD","#A7CBA7","#8DBB8D","#78AE78","#5E9E5E","#479147","#308030","#197519","#016701"]
     cmap = colors.ListedColormap(color_list)
-    # bounds = [1, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5]
-    bounds = list(range(11))
+    bounds = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100]
+    # bounds = list(range(11))
     norm = colors.BoundaryNorm(bounds, cmap.N)
     plt.clf()
     # c = sns.color_palette("light:#006600", as_cmap=True)
-    ax = sns.heatmap(m[i], linewidth=0.5, cmap=cmap, norm=norm, annot=True)
+    labels = np.array([["IEC(C7)", "Triangle-1", "Hexagon-1", "USB-1", "Triangle-2"], 
+                       ["Cylinder-1", "Key-1", "Plug\n(type F)-1", "Audio Jack\n(3.5mm)", "IEC(C13)"],
+                       ["Cylinder-2", "Hexagon-2", "HDMI-1", "Key-2", "Cylinder-3"],
+                       ["Square-1", "Hexagon-3", "Square-2", "Audio jack\n(6.35mm)", "USB-2"],
+                       ["Plug\n(type C)", "Key-3", "Plug\n(type F)-2", "HDMI-2", "Key-4"]])
+    
+    ax = sns.heatmap(m[i], linewidth=0.5, cmap=cmap, norm=norm, annot=labels, fmt="", annot_kws={"fontsize":8})
     ax.set_xticks([])  # Remove x labels
     ax.set_yticks([])  # Remove y labels
+    cbar = ax.collections[0].colorbar
+    cbar.set_ticks([0,1, 2, 3, 4, 5, 6, 7, 8,9,10, 100])
     # plt.title("Collective Learning Process")
 
     ax.set_title(f'Collective Learning Process')
