11cd9146cb55cfab849cc7f60ab07172c9355bf3
Samuel Schneider <samuel.schneider@tum.de>
2023-10-11 19:09:02 +0200

    check status before assigning task to robot
    Former-commit-id: fc5316ca2c2f3188c65b882548ce8c2892af93f1

--- ml_service/run_experiments.py
+++ ml_service/run_experiments.py
@@ -478,7 +478,7 @@ def five_agent_collective():
 
     sc = SVMLearner(450,10,0,True,False, 0.4,True).get_configuration()
     tags = ["5agents_25tasks", "collective"]
-    for n_current_iter in range(14,23):
+    for n_current_iter in range(15,25):
         tasks = {}
         for xxx in modules: 
             tasks["collective-"+str(xxx)+".rsi.ei.tum.de"] = [str(xxx)+"_left"]
