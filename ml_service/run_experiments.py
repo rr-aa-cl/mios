6c1d28fb0c72a21ea12988fc4def44bf23388885
popnut <ge82kop@mytum.de>
2024-05-07 14:05:06 +0200

    code upate
    Former-commit-id: e8e47b27ab592741ab6555c6a5c8f6bee5d2a232

--- ml_service/run_experiments.py
+++ ml_service/run_experiments.py
@@ -1705,3 +1705,113 @@ def demo_automatica(modules):
         input("start again?")
         for s in services:
             s.resume_service()
+            
+            
+def collective25():
+    
+    modules = list_block_1 + list_block_2 + list_U
+    cutoff = {  '001_left': 0.6,   # best solution found *1.2
+                '003_left': 0.6,
+                '004_left': 0.6,
+                '005_left': 0.6, #
+                '006_left': 0.6,
+                '007_left': 0.6,
+                '008_left': 0.6,
+                '010_left': 0.6,
+                '011_left': 0.6,
+                '012_left': 0.6,
+                '009_left': 0.6,
+                '013_left': 0.6,
+                '014_left': 0.6,
+                '015_left': 0.6,
+                '016_left': 0.9,   #
+                '017_left': 0.6,
+                '018_left': 0.6,
+                '021_left': 0.6,
+                '022_left': 0.6,
+                '023_left': 0.6,
+                '024_left': 0.6,
+                '025_left': 0.6,
+                '027_left': 0.6,
+                '028_left': 0.6, # TODO: replace
+                '029_left': 0.6}
+    # sc = SVMLearner(450,10,0,True,False, 0.4,True).get_configuration()
+    sc = SVMLearner(450,10,0,True,False, 0.4,True).get_configuration()
+
+    # tags = ["5agents_25tasks","collective"]
+    
+
+    tags = ["group1","100tasks"]
+        
+        
+    # for n_current_iter in range(29,30): #range(15,25):   (not reserve)
+    for n_current_iter in [0]: # reverse one
+        tasks = {}
+        #tasks = {"collective-014.rsi.ei.tum.de":["014_left"]}  #  do this task at first
+        for xxx in modules: 
+            tasks["collective-"+str(xxx)+".rsi.ei.tum.de"] = [str(xxx)+"_left"]
+        threads = []
+        print("Number of iteration: ", n_current_iter+1)
+        knowledge_source = Knowledge()
+        knowledge_source.kb_location = "collective-001.rsi.ei.tum.de" # None #  
+        knowledge_source.mode = "global"  # None:isolated parallel (no knowledge from theirself)  # "local": has transfer inside agent
+        knowledge_source.scope = [] # TODO: may here add the tag of previous running
+        knowledge_source.scope.extend(tags)
+        knowledge_source.scope.append("n"+str(n_current_iter+1)) # searching for knowledge on the database (only works for the slow pipeline);  e.g. [] search all, 
+        knowledge_source.type = "all"  # all: 
+            
+        dualarm_skills = []
+        move_context = {
+                    "skill": {
+                        "speed": 0.5,
+                        "acc": 1,
+                        "q_g": [0, 0, 0, 0, 0, 0, 0],
+                        "objects": {
+                            "goal_pose": "hold"}},
+                    "control": {
+                        "control_mode": 3},
+                    "user": {
+                        "env_X": [0.001, 0.001, 0.001, 0.001, 0.001, 0.001],
+                        "F_ext_max": [100, 50]}}
+        dualarm_skills.append(("move", "MoveToPoseJoint", move_context))
+        
+        kb = ServerProxy("http://" + knowledge_source.kb_location+ ":8001", allow_none=True)
+        kb.clear_memory()
+
+        threads = []
+        while len(tasks) > 0:
+            robot = list(tasks.keys())[0]
+            insertable = tasks.pop(robot)[0]  #first index because every robot has just one object
+            #TODO: change names
+            container = insertable+"_container" 
+            approach = container+"_approach"
+            pd = InsertionFactory([robot], TimeMetric("insertion", {"time": 5}),
+                                    {"Insertable": insertable, "Container": container,
+                                    "Approach": approach}).get_problem_definition(insertable)
+            if not get_states([insertable[:3]])[0]:
+                print(robot, "is not ready! Skipping task ",insertable)
+                continue
+            if insertable in cutoff:
+                sc.finish_cost = cutoff[insertable]
+            if insertable == "010_left" or insertable == "023_left" or insertable == "027_left":
+                print("increase limits for ",insertable)
+                pd.domain.limits["p2_f_push_z"] = (0,60)
+            dualarm_cmd = {"agent":robot,"port":13000,"skills":dualarm_skills,"sleep":1}
+            threads.append(Thread(target=learn_single_task, args=(robot, pd, sc, tags, n_current_iter, False, knowledge_source.to_dict(), True, 8000, dualarm_cmd)))
+            threads[-1].start()
+            time.sleep(1)
+            server = ServerProxy("http://%s:%s/" %(robot, "8000"))
+            if server.start_telemetry("10.157.175.246", 8004):
+                print("start sending telemetry")
+            while sum([t.is_alive() for t in threads]) >= 5:  # TODO: decide new number, maybe 5 or 25
+                time.sleep(1)
+
+        for t in threads:
+            t.join()
+        tensor_server = ServerProxy("http://10.157.175.246:8004")
+        tensor_server.stop()
+        kb = ServerProxy("http://" + knowledge_source.kb_location+ ":8001", allow_none=True)
+        kb.clear_memory()
+        print("run ", n_current_iter, " finished :)")
+        # TODO: add video recording by call FLO's rpc
+    return "finished :)"
