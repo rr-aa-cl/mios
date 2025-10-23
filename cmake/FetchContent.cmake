2fb0b72c60f2c76afe52bf7b9524a9ef3fb0ecd7
Samuel Schneider <samuel.schneider@tum.de>
2025-10-24 01:11:52 +0200

    include libfranka>0.15.0 dependency (pinocchio)

--- cmake/FetchContent.cmake
+++ cmake/FetchContent.cmake
@@ -1,7 +1,5 @@
-list(APPEND CMAKE_PREFIX_PATH "/opt/openrobots/lib/cmake")
-
 find_package(Eigen3 REQUIRED)
-find_package(pinocchio REQUIRED)
+# find_package(pinocchio REQUIRED)  #lifranka > 0.15
 
 message("################################## ENTERING FETCHCONTENT FILE ##################################")
 if(NOT TARGET Eigen3::Eigen3)
@@ -16,26 +14,41 @@ endif()
 include(FetchContent)
 set(FETCHCONTENT_QUIET OFF)
 set(FETCHCONTENT_BASE_DIR ${CMAKE_SOURCE_DIR}/_deps)
+set(BUILD_EXAMPLES OFF CACHE INTERNAL "No examples")  # needed for libfranka version > 0.15 
+set(BUILD_TESTS OFF CACHE INTERNAL "No examples")
+set(BUILD_DOCUMENTATION OFF CACHE BOOL "Disable documentation")
+set(BUILD_PYTHON_INTERFACE OFF CACHE BOOL "Disable Python bindings")  # neglet eigenpy of pinocchio
+
+FetchContent_Declare(
+    pinocchio
+    GIT_REPOSITORY https://github.com/stack-of-tasks/pinocchio.git
+    GIT_TAG        v3.8.0
+    GIT_SUBMODULES ""
+    )
+FetchContent_MakeAvailable(pinocchio)
+FetchContent_GetProperties(pinocchio)
+#list(APPEND CMAKE_PREFIX_PATH ${pinocchio_BINARY_DIR})  # so libfranka can find pinocchio
+set(pinocchio_DIR ${pinocchio_BINARY_DIR}/cmake CACHE PATH "Direct path to pinocchio build config") # so libfranka can find pinocchio
 
 FetchContent_Declare(
     libfranka
-    #GIT_REPOSITORY https://github.com/frankaemika/libfranka-release.git
+    #GIT_REPOSITORY https://github.com/frankarobotics/libfranka-release.git
+    GIT_REPOSITORY https://github.com/frankarobotics/libfranka.git
     #GIT_TAG upstream/0.9.2)
+    GIT_TAG upstream/0.17.0)
     #GIT_REPOSITORY https://github.com/frankaemika/libfranka-release.git
     #GIT_TAG upstream/0.5.0)
-    GIT_REPOSITORY https://github.com/frankarobotics/libfranka.git
-    GIT_TAG 0.15.3)
+    
     #GIT_REPOSITORY https://github.com/frankaemika/libfranka.git
     #GIT_TAG 0.13.6)
-set(BUILD_EXAMPLES OFF CACHE INTERNAL "No examples")
-set(BUILD_TESTS OFF CACHE INTERNAL "No examples")
+# list(APPEND CMAKE_PREFIX_PATH "/opt/openrobots/lib/cmake")  # libfranka >0.15
+
 
 FetchContent_Declare(
     mirmi_cpp_utils
-    GIT_REPOSITORY https://github.com/SchneiderROS/mirmi_utils
-    GIT_TAG v1.7.2)
+    GIT_REPOSITORY https://gitlab.lrz.de/mirmi-internal/mirmi_utils.git
+    GIT_TAG dev_samu)
 
 set(FETCHCONTENT_UPDATES_DISCONNECTED ON CACHE INTERNAL "")
 
 FetchContent_MakeAvailable(libfranka mirmi_cpp_utils)
-
